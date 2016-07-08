8b711894289e708a7466fdc710dbff9e6828ad48
jonmio <jon.mio@hotmail.com>
2016-07-08 12:07:14 -0400

    Added logs for messages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request = require('request')
 const app = express()
 
 function sendTextMessage(sender, text) {
-    console.log(sender+"********************")
+    console.log(text+"********************")
     console.log ("inside sendTextMessage")
     let messageData = { text:text }
     request({
